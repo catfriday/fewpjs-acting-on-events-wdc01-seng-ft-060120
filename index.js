f8573c637f13f74feb499eba169a85b09a9ffaf6
Catrina Friday <cat.friday@gmail.com>
2020-07-13 18:54:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+//   function moveDodgerRight() {
+//     let rightNumbers = dodger.style.right.replace("px", "");
+//     let reight = parseInt(rightNumbers, 10);
+   
+//     if (right > 0) {
+//       dodger.style.right = `${right - 1}px`;
+//     }
+//   }
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
